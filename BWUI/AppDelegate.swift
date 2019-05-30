c1ad2adc646ea289769f63c5c7b3c99f244276b3
VarunPeddi <varunpeddi491@gmail.com>
2019-05-30 12:10:16 +0530

    Created Storyboard

--- BWUI/AppDelegate.swift
+++ BWUI/AppDelegate.swift
@@ -12,7 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var mainStoryboard:UIStoryboard?
+    var registerStoryboard:UIStoryboard?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -39,6 +40,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    //MARK: - Private Method(s)
+    private func setUpStoryboards() {
+        mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        registerStoryboard = UIStoryboard(name: "Register", bundle: nil)
+    }
+    
+    //MARK: - Class Method(s)
+    class func sharedInstances() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
     }
 
 
